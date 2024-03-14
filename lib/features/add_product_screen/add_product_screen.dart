584256832ae58faedc4efdf6c39f9142bc16b7b9
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-14 22:07:06 +0200

    Add AddProductScreen feature
    Create AddProductScreen

---
+++ lib/features/add_product_screen/add_product_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddProductScreen extends StatelessWidget {
+  const AddProductScreen({super.key});
+  static const routname = 'AddProductScreen ';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

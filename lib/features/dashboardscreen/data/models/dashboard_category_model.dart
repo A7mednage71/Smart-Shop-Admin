0c7847e9eaae41cef310f84b0813a80b50f89abb
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-14 17:05:47 +0200

    Added  DashboardCategoryModel to handle category data for the dashboard screen.

---
+++ lib/features/dashboardscreen/data/models/dashboard_category_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shopsmart_admin_ar/core/services/app_assets.dart';
+import 'package:shopsmart_admin_ar/features/ordersscreen/orders_screen.dart';
+import 'package:shopsmart_admin_ar/features/searchscreen/search_screen.dart';
+
+class DashboardCategoryModel {
+  final String image, title;
+  final void Function() onpress;
+
+  DashboardCategoryModel({
+    required this.image,
+    required this.title,
+    required this.onpress,
+  });
+
+  static List<DashboardCategoryModel> dashboardCategory(BuildContext context) =>
+      [
+        DashboardCategoryModel(
+          image: Appassets.cloud,
+          title: "Add New Product",
+          onpress: () {
+            // Navigator.of(context).pushNamed();
+          },
+        ),
+        DashboardCategoryModel(
+            image: Appassets.shoppingCart,
+            title: "Inspect All Products",
+            onpress: () {
+              Navigator.of(context).pushNamed(SearchScreen.routeName);
+            }),
+        DashboardCategoryModel(
+          image: Appassets.order,
+          title: "View Orders",
+          onpress: () {
+            Navigator.of(context).pushNamed(OrdersScreenFree.routeName);
+          },
+        ),
+      ];
+}

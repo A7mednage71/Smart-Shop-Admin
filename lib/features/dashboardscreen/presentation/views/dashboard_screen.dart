0b27c77222ec2f831844d8be982e14279bc7832b
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-03-14 17:07:12 +0200

    Add DashboardScreen widget with AppBar and GridView

---
+++ lib/features/dashboardscreen/presentation/views/dashboard_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopsmart_admin_ar/core/services/app_assets.dart';
+import 'package:shopsmart_admin_ar/core/utils/themedata/manager/themeData_provider.dart';
+import 'package:shopsmart_admin_ar/core/widgets/title_text.dart';
+import 'package:shopsmart_admin_ar/features/dashboardscreen/data/models/dashboard_category_model.dart';
+import 'package:shopsmart_admin_ar/features/dashboardscreen/presentation/views/widgets/dashboard_item.dart';
+
+class DashboardScreen extends StatefulWidget {
+  static const routeName = '/DashboardScreen';
+  const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => DashboardScreenState();
+}
+
+class DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeDataProvider>(context);
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const TitlesTextWidget(label: "Dashboard Screen"),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(Appassets.shoppingCart),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              themeProvider.settheme(themevalue: !themeProvider.isDarkTheme());
+            },
+            icon: Icon(themeProvider.isDarkTheme()
+                ? Icons.light_mode
+                : Icons.dark_mode),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return DashboardItem(
+              image: DashboardCategoryModel.dashboardCategory(context)[index]
+                  .image,
+              text: DashboardCategoryModel.dashboardCategory(context)[index]
+                  .title,
+              onTap: DashboardCategoryModel.dashboardCategory(context)[index]
+                  .onpress,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}

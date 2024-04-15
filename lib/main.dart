fe51410f75261486320eff18a2bcbf0bb5b91c2c
A7med_Nage7 <ahmdnajkh71@gmail.com>
2024-04-15 02:34:29 +0200

    solve multidex exception

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopsmart_admin_ar/core/manager/product_provider.dart';
